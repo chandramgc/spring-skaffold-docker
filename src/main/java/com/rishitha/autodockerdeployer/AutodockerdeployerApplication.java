48bc831941f11d0ee402e4c63006a6f97ac24992
Girish Mallula <chandramgc@gmail.com>
2022-04-23 16:06:21 -0800

    Spring boot boiler plate

---
+++ src/main/java/com/rishitha/autodockerdeployer/AutodockerdeployerApplication.java
@@ -0,0 +1,13 @@
+package com.rishitha.autodockerdeployer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutodockerdeployerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutodockerdeployerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rishitha/autodockerdeployer/AutodockerdeployerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishitha.autodockerdeployer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutodockerdeployerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
